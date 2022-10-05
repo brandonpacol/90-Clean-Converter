f9eed7e93931064146f7bc0d7677316fd165bba6
brandonpacol <79070027+brandonpacol@users.noreply.github.com>
2022-10-05 12:31:18 -0700

    updated callback

--- index.js
+++ index.js
@@ -55,48 +55,15 @@ app.get('/login', (req, res) => {
     res.redirect(url);
 })
 
-app.get('/callback', (req, res) => {
-    const error = req.query.error;
+app.get('/callback', async (req, res) => {
     const code = req.query.code;
-    const state = req.query.state;
-  
-    if (error) {
-        console.error('Callback Error:', error);
-        res.send(`Callback Error: ${error}`);
-        return;
-    }
-  
-    spotifyApi
-        .authorizationCodeGrant(code)
-        .then(data => {
-            const access_token = data.body['access_token'];
-            const refresh_token = data.body['refresh_token'];
-            const expires_in = data.body['expires_in'];
-
-            spotifyApi.setAccessToken(access_token);
-            spotifyApi.setRefreshToken(refresh_token);
-
-            console.log('access_token:', access_token);
-            console.log('refresh_token:', refresh_token);
-
-            console.log(
-                `Sucessfully retreived access token. Expires in ${expires_in} s.`
-            );
-            res.redirect(homepage);
-
-            setInterval(async () => {
-                const data = await spotifyApi.refreshAccessToken();
-                const access_token = data.body['access_token'];
-
-                console.log('The access token has been refreshed!');
-                console.log('access_token:', access_token);
-                spotifyApi.setAccessToken(access_token);
-            }, expires_in / 2 * 1000);
-        })
-        .catch(error => {
-            console.error('Error getting Tokens:', error);
-            res.send(`Error getting Tokens: ${error}`);
-        });
+    const result = await spotifyApi.authorizationCodeGrant(code);
+    const data = result.body;
+    const access_token = data.access_token;
+    const refresh_token = data.refresh_token;
+    spotifyApi.setAccessToken(access_token);
+    spotifyApi.setRefreshToken(refresh_token);
+    res.redirect(homepage);
 });
 
 app.get('/getPlaylists', async (req, res) => {
